80a802743c70bc5927e9d887043bf734e96a7265
Danci Adina <danciadina@Corina-PC.localdomain>
2019-09-11 20:54:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String masina = "masina mea";
+        String culoare = "galben";
+        byte varsta = 11;
+        float greutate ="875";
+
+    }
+}

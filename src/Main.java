9d0ba4717fdbc0bf46033f928fa72f3cc5a382f3
danciadina <adanci25@gmail.com>
2019-09-15 21:36:20 +0300

    Nume

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        String masina = "masina mea";
-        String culoare = "galben";
-        byte varsta = 11;
-        float greutate ="875";
-
+        String nume = "Adina";
+        System.out.println(nume);
     }
 }
